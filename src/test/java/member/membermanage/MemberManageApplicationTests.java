d8969bd58b76c46e9db3086ade8d3c309f45edd9
hyun98 <hyun0404woo@gmail.com>
2021-12-28 02:45:09 +0900

    Spring INIT

---
+++ src/test/java/member/membermanage/MemberManageApplicationTests.java
@@ -0,0 +1,13 @@
+package member.membermanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberManageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
