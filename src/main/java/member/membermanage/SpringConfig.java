7ced51311da4b11fe0ee20afd651c79c8b063563
Henu <hyun0404woo@gmail.com>
2022-01-03 01:32:40 +0900

    jdbc

--- src/main/java/member/membermanage/SpringConfig.java
+++ src/main/java/member/membermanage/SpringConfig.java
@@ -1,21 +1,32 @@
 package member.membermanage;
 
+import member.membermanage.repository.JdbcTemplateMemberRepository;
 import member.membermanage.repository.MemberRepository;
 import member.membermanage.repository.MemoryMemberRepository;
 import member.membermanage.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
     
+    private final DataSource dataSource;
+    
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    
     @Bean
-    public MemberService memberService(){
+    public MemberService memberService() {
         return new MemberService(memberRepository());
     }
     
     @Bean
-    public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
--- src/test/java/member/membermanage/service/MemberServiceTest.java
+++ src/test/java/member/membermanage/service/MemberServiceTest.java
@@ -1,77 +1,49 @@
 package member.membermanage.service;
 
 import member.membermanage.domain.Member;
+import member.membermanage.repository.MemberRepository;
 import member.membermanage.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-class memberServiceTest {
 
-    MemberService memberService;
-    MemoryMemberRepository memberRepository;
-    
-    @BeforeEach
-    public void beforeEach(){
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-    
-    @AfterEach
-    public void afterEach(){
-        memberRepository.clearStore();
-    }
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
     
     @Test
-    void 회원가입() {
-        //given
+    public void 회원가입() throws Exception {
+        //Given
         Member member = new Member();
-        member.setName("spring");
-        
-        //when
+        member.setName("hello");
+        //When
         Long saveId = memberService.join(member);
-        
-        //then
-        Member findMember = memberService.findOne(saveId).get();
-        assertThat(member.getName()).isEqualTo(findMember.getName());
+        //Then
+        Member findMember = memberRepository.findById(saveId).get();
+        assertEquals(member.getName(), findMember.getName());
     }
     
     @Test
-    void 중복회원예외(){
-        //given
+    public void 중복_회원_예외() throws Exception {
+        //Given
         Member member1 = new Member();
         member1.setName("spring");
         Member member2 = new Member();
         member2.setName("spring");
-        
-        //when
+        //When
         memberService.join(member1);
-        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-
-        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-/*
-
-        try{
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e){
-            assertThat(e.getMessage()).isEqualTo("123123");
-        }
-*/
-
-        //then
-        
-    }
-
-    @Test
-    void findMembers() {
-    }
-
-    @Test
-    void findOne() {
-    }
+        IllegalStateException e = 
+                assertThrows(IllegalStateException.class,
+                () -> memberService.join(member2));//예외가 발생해야 한다. assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+    } 
 }
